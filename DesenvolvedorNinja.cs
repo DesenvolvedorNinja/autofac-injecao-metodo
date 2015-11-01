03f8054cfabc38f7eb6f8456fbb7a41fd594f1ef
Romulo Cianci <rcianci@gmail.com>
2015-11-01 14:17:41 -0200

    Código-fonte do exemplo de injeção usando métodos com Autofac publicado no blog http://desenvolvedor.ninja

---
+++ DesenvolvedorNinja.cs
@@ -0,0 +1,22 @@
+﻿namespace AutofacDemoWin
+{
+    public class DesenvolvedorNinja : IDesenvolvedor
+    {
+        public ILinguagemProgramacao LinguagemProgramacao { get; set; }
+
+        public ILinguagemProgramacao LinguagemProgramacaoSecundaria { get; set; }
+
+        public DesenvolvedorNinja()
+        {
+            
+        }
+
+        public string GetLinguagemProgramacao() => $"Linguagem principal: {LinguagemProgramacao.Nome} - Linguagem secundáriaa: {LinguagemProgramacaoSecundaria.Nome}";
+
+        public void SetLinguagemProgramacao(ILinguagemProgramacao linguagemProgramacao, ILinguagemProgramacao linguagemProgramacaoSecundaria)
+        {
+            LinguagemProgramacao = linguagemProgramacao;
+            LinguagemProgramacaoSecundaria = linguagemProgramacaoSecundaria;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AutofacDemoWin
+{
+    public partial class FormAutofacDemoWin : Form
+    {
+        private IContainer _container;
+
+        public FormAutofacDemoWin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAutofacDemoWin_Load(object sender, EventArgs e)
+        {
+            var builder = new ContainerBuilder();
+            
+            builder.Register(c =>
+            {
+                var result = new DesenvolvedorNinja();
+                var depPrincipal = c.Resolve<ILinguagemProgramacao>();
+                var depSecundaria = c.Resolve<LinguagemGO>();
+                result.SetLinguagemProgramacao(depPrincipal, depSecundaria);
+                return result;
+            }).As<IDesenvolvedor>().InstancePerDependency();
+            
+            /*
+            builder.RegisterType<DesenvolvedorNinja>().OnActivating(c => {
+                var depPrincipal = c.Context.Resolve<ILinguagemProgramacao>();
+                var depSecundaria = c.Context.Resolve<LinguagemGO>();
+                c.Instance.SetLinguagemProgramacao(depPrincipal, depSecundaria);
+            }).As<IDesenvolvedor>().InstancePerDependency();
+            */
+            builder.RegisterType<LinguagemCSharp>().As<ILinguagemProgramacao>().AsSelf().InstancePerDependency();
+            builder.RegisterType<LinguagemGO>().AsSelf().InstancePerDependency();
+            _container = builder.Build();
+        }
+
+        private void buttonObterLinguagemAutofac_Click(object sender, EventArgs e)
+        {
+            IDesenvolvedor desenvolvedor = _container.Resolve<IDesenvolvedor>();
+            MessageBox.Show(desenvolvedor.GetLinguagemProgramacao());
+        }
+    }
+}
